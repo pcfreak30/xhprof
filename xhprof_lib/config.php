8995e975e6a9ccc95a990671a8e217bf2c498014
Derrick Hammer <derrick@derrickhammer.com>
2019-04-20 03:15:10 -0400

    *Auto configure for wordpress when detected
    * disable IP control
    * prevent xhprof from recording itself.

--- xhprof_lib/config.php
+++ xhprof_lib/config.php
@@ -1,24 +1,31 @@
 <?php
 $_xhprof = array();
-
 // Change these:
-$_xhprof['dbtype'] = 'mysql'; // Only relevant for PDO
-$_xhprof['dbhost'] = 'localhost';
-$_xhprof['dbuser'] = 'root';
-$_xhprof['dbpass'] = 'password';
-$_xhprof['dbname'] = 'xhprof';
-$_xhprof['dbadapter'] = 'Pdo';
+$_xhprof['dbtype']     = 'mysql'; // Only relevant for PDO
+$_xhprof['dbhost']     = 'localhost';
+$_xhprof['dbuser']     = 'root';
+$_xhprof['dbpass']     = 'password';
+$_xhprof['dbname']     = 'xhprof';
+$_xhprof['dbadapter']  = 'Pdo';
 $_xhprof['servername'] = 'a';
-$_xhprof['namespace'] = 'myapp';
-$_xhprof['url'] = '/xhprof/xhprof_html';
-$_xhprof['getparam'] = "_profile";
+$_xhprof['namespace']  = 'myapp';
+$_xhprof['url']        = '/xhprof/xhprof_html';
+$_xhprof['getparam']   = "_profile";
+
+$wp_config = dirname( dirname( __DIR__ ) ) . '/wp-config.php')
+if ( file_exists( $wp_config ) ) {
+	include $wp_config;
+	$_xhprof['dbhost'] = DB_NAME;
+	$_xhprof['dbuser'] = DB_USER;
+	$_xhprof['dbpass'] = DB_PASSWORD;
+}
 
 /*
  * MySQL/MySQLi/PDO ONLY
  * Switch to JSON for better performance and support for larger profiler data sets.
  * WARNING: Will break with existing profile data, you will need to TRUNCATE the profile data table.
  */
-$_xhprof['serializer'] = 'php'; 
+$_xhprof['serializer'] = 'php';
 
 //Uncomment one of these, platform dependent. You may need to tune for your specific environment, but they're worth a try
 
@@ -42,19 +49,19 @@ $ignoreDomains = array();
 
 $exceptionURLs = array();
 
-$exceptionPostURLs = array();
-$exceptionPostURLs[] = "login";
+$exceptionPostURLs   = array();
+$exceptionPostURLs[] = "xhprof_html";
 
 
-$_xhprof['display'] = false;
+$_xhprof['display']   = false;
 $_xhprof['doprofile'] = false;
 
 //Control IPs allow you to specify which IPs will be permitted to control when profiling is on or off within your application, and view the results via the UI.
-// $controlIPs = false; //Disables access controlls completely. 
-$controlIPs = array();
-$controlIPs[] = "127.0.0.1";   // localhost, you'll want to add your own ip here
-$controlIPs[] = "::1";         // localhost IP v6
-$controlIPs = false;
+$controlIPs = false; //Disables access controlls completely.
+//$controlIPs   = array();
+//$controlIPs[] = "127.0.0.1";   // localhost, you'll want to add your own ip here
+//$controlIPs[] = "::1";         // localhost IP v6
+//$controlIPs   = false;
 //$otherURLS = array();
 
 // ignore builtin functions and call_user_func* during profiling
@@ -63,78 +70,71 @@ $controlIPs = false;
 //Default weight - can be overidden by an Apache environment variable 'xhprof_weight' for domain-specific values
 $weight = 100;
 
-if($domain_weight = getenv('xhprof_weight')) {
+if ( $domain_weight = getenv( 'xhprof_weight' ) ) {
 	$weight = $domain_weight;
 }
 
-unset($domain_weight);
+unset( $domain_weight );
 
   /**
-  * The goal of this function is to accept the URL for a resource, and return a "simplified" version
-  * thereof. Similar URLs should become identical. Consider:
-  * http://example.org/stories.php?id=2323
-  * http://example.org/stories.php?id=2324
-  * Under most setups these two URLs, while unique, will have an identical execution path, thus it's
-  * worthwhile to consider them as identical. The script will store both the original URL and the
-  * Simplified URL for display and comparison purposes. A good simplified URL would be:
-  * http://example.org/stories.php?id=
-  * 
-  * @param string $url The URL to be simplified
-  * @return string The simplified URL 
-  */
-  function _urlSimilartor($url)
-  {
-      //This is an example 
-      $url = preg_replace("!\d{4}!", "", $url);
-      
-      // For domain-specific configuration, you can use Apache setEnv xhprof_urlSimilartor_include [some_php_file]
-      if($similartorinclude = getenv('xhprof_urlSimilartor_include')) {
-      	require_once($similartorinclude);
-      }
-      
-      $url = preg_replace("![?&]_profile=\d!", "", $url);
-      return $url;
-  }
-  
-  function _aggregateCalls($calls, $rules = null)
-  {
-    $rules = array(
-        'Loading' => 'load::',
-        'mysql' => 'mysql_'
-        );
+   * The goal of this function is to accept the URL for a resource, and return a "simplified" version
+   * thereof. Similar URLs should become identical. Consider:
+   * http://example.org/stories.php?id=2323
+   * http://example.org/stories.php?id=2324
+   * Under most setups these two URLs, while unique, will have an identical execution path, thus it's
+   * worthwhile to consider them as identical. The script will store both the original URL and the
+   * Simplified URL for display and comparison purposes. A good simplified URL would be:
+   * http://example.org/stories.php?id=
+   *
+   * @param string $url The URL to be simplified
+   *
+   * @return string The simplified URL
+   */
+  function _urlSimilartor( $url ) {
+	  //This is an example
+	  $url = preg_replace( "!\d{4}!", "", $url );
 
-    // For domain-specific configuration, you can use Apache setEnv xhprof_aggregateCalls_include [some_php_file]
-  	if(isset($run_details['aggregateCalls_include']) && strlen($run_details['aggregateCalls_include']) > 1)
-		{
-    	require_once($run_details['aggregateCalls_include']);
-		}        
-        
-    $addIns = array();
-    foreach($calls as $index => $call)
-    {
-        foreach($rules as $rule => $search)
-        {
-            if (strpos($call['fn'], $search) !== false)
-            {
-                if (isset($addIns[$search]))
-                {
-                    unset($call['fn']);
-                    foreach($call as $k => $v)
-                    {
-                        $addIns[$search][$k] += $v;
-                    }
-                }else
-                {
-                    $call['fn'] = $rule;
-                    $addIns[$search] = $call;
-                }
-                unset($calls[$index]);  //Remove it from the listing
-                break;  //We don't need to run any more rules on this
-            }else
-            {
-                //echo "nomatch for $search in {$call['fn']}<br />\n";
-            }
-        }
-    }
-    return array_merge($addIns, $calls);
+	  // For domain-specific configuration, you can use Apache setEnv xhprof_urlSimilartor_include [some_php_file]
+	  if ( $similartorinclude = getenv( 'xhprof_urlSimilartor_include' ) ) {
+		  require_once( $similartorinclude );
+	  }
+
+	  $url = preg_replace( "![?&]_profile=\d!", "", $url );
+
+	  return $url;
+  }
+
+  function _aggregateCalls( $calls, $rules = null ) {
+	  $rules = array(
+		  'Loading' => 'load::',
+		  'mysql'   => 'mysql_',
+	  );
+
+	  // For domain-specific configuration, you can use Apache setEnv xhprof_aggregateCalls_include [some_php_file]
+	  if ( isset( $run_details['aggregateCalls_include'] ) && strlen( $run_details['aggregateCalls_include'] ) > 1 ) {
+		  require_once( $run_details['aggregateCalls_include'] );
+	  }
+
+	  $addIns = array();
+	  foreach ( $calls as $index => $call ) {
+		  foreach ( $rules as $rule => $search ) {
+			  if ( strpos( $call['fn'], $search ) !== false ) {
+				  if ( isset( $addIns[ $search ] ) ) {
+					  unset( $call['fn'] );
+					  foreach ( $call as $k => $v ) {
+						  $addIns[ $search ][ $k ] += $v;
+					  }
+				  } else {
+					  $call['fn']        = $rule;
+					  $addIns[ $search ] = $call;
+				  }
+				  unset( $calls[ $index ] );  //Remove it from the listing
+				  break;  //We don't need to run any more rules on this
+			  } else {
+				  //echo "nomatch for $search in {$call['fn']}<br />\n";
+			  }
+		  }
+	  }
+
+	  return array_merge( $addIns, $calls );
   }
