1dd523f914f1c7d25c2fc27553f6c478842d1ef2
Derrick Hammer <derrick@derrickhammer.com>
2019-04-20 07:48:06 +0000

    *Fix syntax error

--- xhprof_lib/config.php
+++ xhprof_lib/config.php
@@ -12,7 +12,7 @@ $_xhprof['namespace']  = 'myapp';
 $_xhprof['url']        = '/xhprof/xhprof_html';
 $_xhprof['getparam']   = "_profile";
 
-$wp_config = dirname( dirname( __DIR__ ) ) . '/wp-config.php');
+$wp_config = dirname( dirname( __DIR__ ) ) . '/wp-config.php';
 if ( file_exists( $wp_config ) ) {
 	include $wp_config;
 	$_xhprof['dbhost'] = DB_HOST;
