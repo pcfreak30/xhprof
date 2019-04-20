84b1e294cdb6a8991f8c814f32a5ec2f140801f9
Derrick Hammer <derrick@derrickhammer.com>
2019-04-20 03:25:15 -0400

    *Auto create database table if it does not exist

--- xhprof_lib/utils/Db/Pdo.php
+++ xhprof_lib/utils/Db/Pdo.php
@@ -49,6 +49,34 @@ class Db_Pdo extends Db_Abstract
         }
         $this->db = $db;
         $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $tables = $this->db->query("SHOW TABLES LIKE 'details';");
+        $tables = $tables->fetchAll();
+        if(empty($tables)){
+          $this->db->query("CREATE TABLE `details` (
+ `id` char(17) NOT NULL,
+ `url` varchar(255) default NULL,
+ `c_url` varchar(255) default NULL,
+ `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+ `server name` varchar(64) default NULL,
+ `perfdata` MEDIUMBLOB,
+ `type` tinyint(4) default NULL,
+ `cookie` BLOB,
+ `post` BLOB,
+ `get` BLOB,
+ `pmu` int(11) unsigned default NULL,
+ `wt` int(11) unsigned default NULL,
+ `cpu` int(11) unsigned default NULL,
+ `server_id` char(3) NOT NULL default 't11',
+ `aggregateCalls_include` varchar(255) DEFAULT NULL,
+ PRIMARY KEY  (`id`),
+ KEY `url` (`url`),
+ KEY `c_url` (`c_url`),
+ KEY `cpu` (`cpu`),
+ KEY `wt` (`wt`),
+ KEY `pmu` (`pmu`),
+ KEY `timestamp` (`timestamp`)
+ ) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
+        }
     }
     
     public function query($sql)
