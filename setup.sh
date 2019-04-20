a0fcfa4f5e3e57cd28c5f82e3fcaf6a598c08e32
Derrick Hammer <derrick@derrickhammer.com>
2019-04-20 07:40:44 +0000

    *Add fixes

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo auto_prepend_file = "$(dirname $0)/external/header.php" > $(dirname $PWD)/.user.ini
+echo auto_prepend_file = "'$(dirname $(readlink -f $0))/external/header.php'" > $PWD/.user.ini
